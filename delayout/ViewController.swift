b878e1960b033526a4ee6003832b953f7309ebe7
Bruno Rocha <mail.brunorocha@gmail.com>
2019-06-29 13:04:11 -0300

    Initial Commit

---
+++ delayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delayout
+//
+//  Created by Bruno Rocha on 29/06/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
